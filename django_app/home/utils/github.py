2ea25c06b25c5cabb239ed86c37605fd3027543c
aarav <aaravpradhan12365@gmail.com>
2025-03-08 12:05:15 +0545

    added dummy code for testing in utils

---
+++ django_app/home/utils/github.py
@@ -0,0 +1,11 @@
+def get_pr_info(repo_url, pr_number, github_token):
+    """
+    A dummy function that returns dummy data
+    """
+    return {
+        'title': 'Dummy PR title',
+        'body': 'Dummy PR body',
+        'author': 'Dummy author',
+        'labels': ['dummy-label'],
+        'state': 'open'
+    }
